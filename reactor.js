7274392dbc10bd86cfebb6aa7b6969c5038504c1
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 16:53:20 +0200

    Update reactor.js

--- reactor.js
+++ reactor.js
@@ -7,26 +7,22 @@ class NuclearReactor {
         this.adjustInterval = null;
     }
 
-    // De fysica achter de simulatie
     calculatePhysics() {
         if (!this.isScram) {
-            // Target temperatuur stijgt naarmate de staven verder eruit zijn (rods up)
             let targetTemp = 285 + (this.rods * 6.5);
-            // Traagheid: de temperatuur kruipt naar de target toe
-            this.temp += (targetTemp - this.temp) * 0.02;
+            this.temp += (targetTemp - this.temp) * 0.02; // Traagheid
         } else {
-            // Snelle afkoeling na noodstop
-            this.temp += (285 - this.temp) * 0.04;
+            this.temp += (285 - this.temp) * 0.04; // Afkoeling
             if (this.temp < 290) {
                 AudioSystem.stopAlarm();
-                this.isScram = false; // Reset status als het veilig is
+                this.isScram = false; 
             }
         }
     }
 
     startAdjust(val) {
         if (this.isScram) return;
-        AudioSystem.init(); // Activeer audio context door gebruikersinteractie
+        AudioSystem.init(); // Browser audio unlock
         this.adjustInterval = setInterval(() => {
             this.rods = Math.min(Math.max(this.rods + val, 0), 100);
         }, 50);
@@ -40,7 +36,7 @@ class NuclearReactor {
         this.isScram = true;
         this.rods = 0;
         AudioSystem.playAlarm();
-        this.log("!!! EMERGENCY SHUTDOWN (SCRAM) ACTIVATED !!!");
+        this.log("!!! EMERGENCY SHUTDOWN (SCRAM) !!!");
     }
 
     log(msg) {
