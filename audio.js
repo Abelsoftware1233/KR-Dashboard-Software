c4be9465f571f1905ef0a7667eb03f8f7da4f919
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 18:30:38 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,40 +1,35 @@
 const AudioSystem = {
     ctx: null,
     oscillator: null,
-    gain: null,
 
     init() {
-        if (!this.ctx) {
-            this.ctx = new (window.AudioContext || window.webkitAudioContext)();
-        }
+        this.ctx = new (window.AudioContext || window.webkitAudioContext)();
     },
 
-    play() {
-        this.init();
+    playAlarm() {
+        if (!this.ctx) this.init();
         if (this.oscillator) return;
-        
+
         this.oscillator = this.ctx.createOscillator();
-        this.gain = this.ctx.createGain();
+        const gain = this.ctx.createGain();
+
+        this.oscillator.type = 'triangle';
+        this.oscillator.frequency.setValueAtTime(440, this.ctx.currentTime);
+        this.oscillator.frequency.exponentialRampToValueAtTime(880, this.ctx.currentTime + 0.5);
+        this.oscillator.loop = true;
+
+        gain.gain.setValueAtTime(0.1, this.ctx.currentTime);
         
-        this.oscillator.type = 'sawtooth';
-        this.oscillator.frequency.setValueAtTime(400, this.ctx.currentTime);
-        this.oscillator.frequency.exponentialRampToValueAtTime(800, this.ctx.currentTime + 0.5);
-        
-        this.gain.gain.setValueAtTime(0.05, this.ctx.currentTime);
-        this.oscillator.connect(this.gain);
-        this.gain.connect(this.ctx.destination);
+        this.oscillator.connect(gain);
+        gain.connect(this.ctx.destination);
+
         this.oscillator.start();
-        
-        this.interval = setInterval(() => {
-            if (!this.oscillator) return;
-            this.oscillator.frequency.setValueAtTime(400, this.ctx.currentTime);
-            this.oscillator.frequency.exponentialRampToValueAtTime(800, this.ctx.currentTime + 0.5);
-        }, 600);
+        // Laat de frequentie op en neer gaan (sirene effect)
+        this.oscillator.frequency.setTargetAtTime(440, this.ctx.currentTime, 0.5);
     },
 
-    stop() {
+    stopAlarm() {
         if (this.oscillator) {
-            clearInterval(this.interval);
             this.oscillator.stop();
             this.oscillator = null;
         }
