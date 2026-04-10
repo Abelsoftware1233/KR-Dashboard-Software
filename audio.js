7de133f89b9ac0b39bbae286da73205bca54d5cf
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 16:50:40 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,35 +1,45 @@
 const AudioSystem = {
     ctx: null,
     oscillator: null,
+    gainNode: null,
 
     init() {
+        if (this.ctx) return;
         this.ctx = new (window.AudioContext || window.webkitAudioContext)();
     },
 
     playAlarm() {
-        if (!this.ctx) this.init();
+        this.init();
         if (this.oscillator) return;
 
         this.oscillator = this.ctx.createOscillator();
-        const gain = this.ctx.createGain();
+        this.gainNode = this.ctx.createGain();
 
-        this.oscillator.type = 'triangle';
-        this.oscillator.frequency.setValueAtTime(440, this.ctx.currentTime);
-        this.oscillator.frequency.exponentialRampToValueAtTime(880, this.ctx.currentTime + 0.5);
-        this.oscillator.loop = true;
-
-        gain.gain.setValueAtTime(0.1, this.ctx.currentTime);
+        this.oscillator.type = 'sawtooth';
+        this.oscillator.frequency.setValueAtTime(300, this.ctx.currentTime);
         
-        this.oscillator.connect(gain);
-        gain.connect(this.ctx.destination);
-
+        // Maak een pulserend sirene-effect
+        this.oscillator.frequency.linearRampToValueAtTime(600, this.ctx.currentTime + 0.5);
+        this.oscillator.frequency.linearRampToValueAtTime(300, this.ctx.currentTime + 1.0);
+        
+        this.gainNode.gain.setValueAtTime(0.05, this.ctx.currentTime);
+        
+        this.oscillator.connect(this.gainNode);
+        this.gainNode.connect(this.ctx.destination);
+        
         this.oscillator.start();
-        // Laat de frequentie op en neer gaan (sirene effect)
-        this.oscillator.frequency.setTargetAtTime(440, this.ctx.currentTime, 0.5);
+        
+        // Herhaal het sirene-effect
+        this.freqInterval = setInterval(() => {
+            if (!this.oscillator) return;
+            this.oscillator.frequency.linearRampToValueAtTime(600, this.ctx.currentTime + 0.5);
+            this.oscillator.frequency.linearRampToValueAtTime(300, this.ctx.currentTime + 1.0);
+        }, 1000);
     },
 
     stopAlarm() {
         if (this.oscillator) {
+            clearInterval(this.freqInterval);
             this.oscillator.stop();
             this.oscillator = null;
         }
