68d0d4b27bb2e889505b2f76c316d6286ea2422e
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 18:54:50 +0200

    Update reactor.js

--- reactor.js
+++ reactor.js
@@ -3,24 +3,42 @@ class NuclearReactor {
         this.id = id;
         this.temp = 285.0;
         this.rods = 0.0;
+        this.power = 0.0;
         this.isScram = false;
+        this.isMeltdown = false;
         this.adjustInterval = null;
     }
 
     calculatePhysics() {
         if (!this.isScram) {
+            // Natuurlijke hitte opbouw op basis van regelstaven
             let targetTemp = 285 + (this.rods * 5.2);
+            // Geleidelijke verandering (traagheid)
             this.temp += (targetTemp - this.temp) * 0.05;
+            this.power = this.rods * 15.2;
         } else {
+            // Afkoelen na SCRAM
             this.temp += (285 - this.temp) * 0.02;
-            if (this.temp < 287) AudioSystem.stopAlarm();
+            this.power *= 0.5;
+            
+            // Auto-reset SCRAM status als de temperatuur veilig is
+            if (this.temp < 290) {
+                this.isScram = false;
+                this.log("System stabilized. Ready for restart.");
+            }
+        }
+
+        // Meltdown check
+        if (this.temp > 800) {
+            this.isMeltdown = true;
         }
     }
 
     startAdjust(val) {
-        if (this.isScram) return;
+        if (this.isScram || this.isMeltdown) return;
         this.adjustInterval = setInterval(() => {
             this.rods = Math.min(Math.max(this.rods + val, 0), 100);
+            // Optioneel: log elke stap of elke 10%
         }, 50);
     }
 
@@ -31,12 +49,18 @@ class NuclearReactor {
     scram() {
         this.isScram = true;
         this.rods = 0;
-        AudioSystem.playAlarm();
-        this.log("!!! EMERGENCY SHUTDOWN !!!");
+        this.log("!!! EMERGENCY SHUTDOWN ACTIVATED !!!");
+        // Visuele feedback (flits)
+        document.body.style.backgroundColor = "#200";
+        setTimeout(() => { document.body.style.backgroundColor = "#000"; }, 200);
     }
 
     log(msg) {
         const log = document.getElementById('event-log');
-        log.innerHTML = `> ${msg}<br>` + log.innerHTML;
+        if (log) {
+            log.innerHTML = `> ${msg}<br>` + log.innerHTML;
+            // Houd de log compact
+            if (log.children.length > 20) log.removeChild(log.lastChild);
+        }
     }
 }
