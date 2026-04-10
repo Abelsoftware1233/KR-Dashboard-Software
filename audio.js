b1d9bf17b464e7bad746a2b2b3c87782d1231601
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 16:53:03 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -14,22 +14,18 @@ const AudioSystem = {
 
         this.oscillator = this.ctx.createOscillator();
         this.gainNode = this.ctx.createGain();
-
         this.oscillator.type = 'sawtooth';
         this.oscillator.frequency.setValueAtTime(300, this.ctx.currentTime);
         
-        // Maak een pulserend sirene-effect
+        // Pulserend effect
         this.oscillator.frequency.linearRampToValueAtTime(600, this.ctx.currentTime + 0.5);
         this.oscillator.frequency.linearRampToValueAtTime(300, this.ctx.currentTime + 1.0);
         
         this.gainNode.gain.setValueAtTime(0.05, this.ctx.currentTime);
-        
         this.oscillator.connect(this.gainNode);
         this.gainNode.connect(this.ctx.destination);
-        
         this.oscillator.start();
         
-        // Herhaal het sirene-effect
         this.freqInterval = setInterval(() => {
             if (!this.oscillator) return;
             this.oscillator.frequency.linearRampToValueAtTime(600, this.ctx.currentTime + 0.5);
