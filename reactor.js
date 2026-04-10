97e5b9d9f357edc2300fd938d3f0eabc2b09c689
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 18:31:44 +0200

    Update reactor.js

--- reactor.js
+++ reactor.js
@@ -1,36 +1,24 @@
 class NuclearReactor {
-    constructor() {
+    constructor(id) {
+        this.id = id;
         this.temp = 285.0;
         this.rods = 0.0;
-        this.pumpsActive = false;
         this.isScram = false;
-        this.isMeltdown = false;
         this.adjustInterval = null;
     }
 
     calculatePhysics() {
-        if (this.isMeltdown) return;
-
-        // Target temperatuur berekenen
-        let targetTemp = 285 + (this.rods * 6.0);
-        if (this.pumpsActive) {
-            targetTemp -= (this.rods * 3.5); // Koeling vermindert de hitte
-        }
-
-        // Geleidelijke verandering (Inertia)
-        let diff = targetTemp - this.temp;
-        this.temp += diff * 0.03;
-
-        // Meltdown grens
-        if (this.temp > 620 && !this.isMeltdown) {
-            this.isMeltdown = true;
-            this.log("!!! CRITICAL FAILURE: REACTOR MELTDOWN !!!");
+        if (!this.isScram) {
+            let targetTemp = 285 + (this.rods * 5.2);
+            this.temp += (targetTemp - this.temp) * 0.05;
+        } else {
+            this.temp += (285 - this.temp) * 0.02;
+            if (this.temp < 287) AudioSystem.stopAlarm();
         }
     }
 
     startAdjust(val) {
-        if (this.isScram || this.isMeltdown) return;
-        if (this.adjustInterval) clearInterval(this.adjustInterval);
+        if (this.isScram) return;
         this.adjustInterval = setInterval(() => {
             this.rods = Math.min(Math.max(this.rods + val, 0), 100);
         }, 50);
@@ -38,41 +26,17 @@ class NuclearReactor {
 
     stopAdjust() {
         clearInterval(this.adjustInterval);
-        this.adjustInterval = null;
-    }
-
-    togglePump(id) {
-        if (this.isMeltdown) return;
-        this.pumpsActive = !this.pumpsActive;
-        const label = document.getElementById(`pump${id}-label`);
-        const btn = document.getElementById(`pump${id}-btn`);
-        
-        if (this.pumpsActive) {
-            btn.classList.add('active');
-            label.innerText = "RUNNING";
-            label.className = "pumps-label on";
-            this.log("Secondary cooling pump engaged.");
-        } else {
-            btn.classList.remove('active');
-            label.innerText = "OFF";
-            label.className = "pumps-label off";
-            this.log("Secondary cooling pump disengaged.");
-        }
     }
 
     scram() {
         this.isScram = true;
         this.rods = 0;
-        this.log("!!! EMERGENCY SHUTDOWN (SCRAM) INITIATED !!!");
-        // AudioSystem wordt aangeroepen vanuit main.js voor betere sync
+        AudioSystem.playAlarm();
+        this.log("!!! EMERGENCY SHUTDOWN !!!");
     }
 
     log(msg) {
         const log = document.getElementById('event-log');
-        const time = new Date().toLocaleTimeString();
-        log.innerHTML = `<div class="log-entry">[${time}] ${msg}</div>` + log.innerHTML;
+        log.innerHTML = `> ${msg}<br>` + log.innerHTML;
     }
 }
-
-// Maak de instantie aan
-const reactor = new NuclearReactor();
