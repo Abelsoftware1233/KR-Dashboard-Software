60fbc0f08d2559f4e562bc50deec935359e4e53d
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 18:58:12 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,37 +1,71 @@
+/**
+ * AUDIO SYSTEM - audio.js
+ * Beheert de alarmgeluiden voor de reactor simulator
+ */
 const AudioSystem = {
     ctx: null,
     oscillator: null,
+    gainNode: null,
+    isLooping: false,
 
     init() {
-        this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+        // AudioContext kan alleen starten na een gebruikersinteractie (klik)
+        if (!this.ctx) {
+            this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+        }
     },
 
     playAlarm() {
-        if (!this.ctx) this.init();
-        if (this.oscillator) return;
+        this.init();
+        if (this.oscillator || this.isLooping) return;
+
+        this.isLooping = true;
+        this.createSiren();
+    },
+
+    createSiren() {
+        if (!this.isLooping) return;
 
         this.oscillator = this.ctx.createOscillator();
-        const gain = this.ctx.createGain();
+        this.gainNode = this.ctx.createGain();
 
-        this.oscillator.type = 'triangle';
-        this.oscillator.frequency.setValueAtTime(440, this.ctx.currentTime);
-        this.oscillator.frequency.exponentialRampToValueAtTime(880, this.ctx.currentTime + 0.5);
-        this.oscillator.loop = true;
-
-        gain.gain.setValueAtTime(0.1, this.ctx.currentTime);
+        // Type 'sawtooth' of 'triangle' werkt het best voor alarmen
+        this.oscillator.type = 'sawtooth'; 
         
-        this.oscillator.connect(gain);
-        gain.connect(this.ctx.destination);
+        // Startfrequentie
+        this.oscillator.frequency.setValueAtTime(300, this.ctx.currentTime);
+        // Glij naar hoge frequentie (sirene effect)
+        this.oscillator.frequency.exponentialRampToValueAtTime(600, this.ctx.currentTime + 0.8);
+
+        this.gainNode.gain.setValueAtTime(0.05, this.ctx.currentTime);
+        // Fade out aan het einde van de puls
+        this.gainNode.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + 0.9);
+
+        this.oscillator.connect(this.gainNode);
+        this.gainNode.connect(this.ctx.destination);
 
         this.oscillator.start();
-        // Laat de frequentie op en neer gaan (sirene effect)
-        this.oscillator.frequency.setTargetAtTime(440, this.ctx.currentTime, 0.5);
+        this.oscillator.stop(this.ctx.currentTime + 1);
+
+        // Herhaal de puls zolang het alarm aan staat
+        this.oscillator.onended = () => {
+            if (this.isLooping) {
+                this.createSiren();
+            }
+        };
     },
 
     stopAlarm() {
-        if (this.oscillator) {
-            this.oscillator.stop();
-            this.oscillator = null;
+        this.isLooping = false;
+        if (this.gainNode) {
+            // Laat het laatste geluidje netjes uitfaden
+            this.gainNode.gain.setTargetAtTime(0, this.ctx.currentTime, 0.1);
         }
+        setTimeout(() => {
+            if (this.oscillator) {
+                this.oscillator.stop();
+                this.oscillator = null;
+            }
+        }, 200);
     }
 };
