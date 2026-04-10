0d80a3760e81cfc9b6ec05856425d03a8c74d6a2
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-04-10 17:31:30 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,66 +1,42 @@
 const AudioSystem = {
     ctx: null,
     oscillator: null,
-    gainNode: null,
-    isManualSilence: false,
+    gain: null,
 
     init() {
-        if (this.ctx) return;
-        this.ctx = new (window.AudioContext || window.webkitAudioContext)();
-    },
-
-    playAlarm() {
-        if (this.isManualSilence) return; // Alarm is handmatig uitgeschakeld
-        this.init();
-        if (this.oscillator) return;
-
-        this.oscillator = this.ctx.createOscillator();
-        this.gainNode = this.ctx.createGain();
-
-        this.oscillator.type = 'sawtooth';
-        this.oscillator.frequency.setValueAtTime(440, this.ctx.currentTime);
-        this.gainNode.gain.setValueAtTime(0.06, this.ctx.currentTime);
-        
-        // Pulserend sirene effect (frequentie op en neer)
-        this.oscillator.frequency.linearRampToValueAtTime(880, this.ctx.currentTime + 0.4);
-        this.oscillator.frequency.linearRampToValueAtTime(440, this.ctx.currentTime + 0.8);
-        
-        this.oscillator.connect(this.gainNode);
-        this.gainNode.connect(this.ctx.destination);
-        
-        this.oscillator.start();
-        
-        // Loop het sirene effect
-        this.freqInterval = setInterval(() => {
-            if (!this.oscillator) return;
-            this.oscillator.frequency.linearRampToValueAtTime(880, this.ctx.currentTime + 0.4);
-            this.oscillator.frequency.linearRampToValueAtTime(440, this.ctx.currentTime + 0.8);
-        }, 800);
-    },
-
-    stopAlarm() {
-        if (this.oscillator) {
-            clearInterval(this.freqInterval);
-            this.oscillator.stop();
-            this.oscillator = null;
+        if (!this.ctx) {
+            this.ctx = new (window.AudioContext || window.webkitAudioContext)();
         }
     },
 
-    toggleAlarmManual() {
-        this.init(); // Browser audio unlock
-        const btn = document.getElementById('alarm-btn');
+    play() {
+        this.init();
+        if (this.oscillator) return;
+        
+        this.oscillator = this.ctx.createOscillator();
+        this.gain = this.ctx.createGain();
+        
+        this.oscillator.type = 'sawtooth';
+        this.oscillator.frequency.setValueAtTime(400, this.ctx.currentTime);
+        this.oscillator.frequency.exponentialRampToValueAtTime(800, this.ctx.currentTime + 0.5);
+        
+        this.gain.gain.setValueAtTime(0.05, this.ctx.currentTime);
+        this.oscillator.connect(this.gain);
+        this.gain.connect(this.ctx.destination);
+        this.oscillator.start();
+        
+        this.interval = setInterval(() => {
+            if (!this.oscillator) return;
+            this.oscillator.frequency.setValueAtTime(400, this.ctx.currentTime);
+            this.oscillator.frequency.exponentialRampToValueAtTime(800, this.ctx.currentTime + 0.5);
+        }, 600);
+    },
+
+    stop() {
         if (this.oscillator) {
-            // Alarm staat aan, zet handmatig uit (silence)
-            this.isManualSilence = true;
-            this.stopAlarm();
-            btn.innerText = "ALARM ACTIVATED";
-            btn.className = "btn btn-alarm active-silence"; // Groen
-        } else {
-            // Silence opheffen, of handmatig alarm aanzetten
-            this.isManualSilence = false;
-            btn.innerText = "ALARM SILENCE";
-            btn.className = "btn btn-alarm"; // Amber
-            // Als de reactor nog steeds kritiek is, zal de main.js het direct weer starten
+            clearInterval(this.interval);
+            this.oscillator.stop();
+            this.oscillator = null;
         }
     }
 };
